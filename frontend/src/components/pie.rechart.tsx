e90bc709a4a414f713ef2aa67b8656d0feb84e7c
Benoit Botton <benoit.botton@zerdalab.com>
2022-09-09 22:30:08 +0100

    PieRechartComponent now a class

--- frontend/src/components/pie.rechart.tsx
+++ frontend/src/components/pie.rechart.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
 
 type pieData = {
@@ -6,47 +6,50 @@ type pieData = {
   value: number
 }[]
 
-function PieRechartComponent ({ pieData }: { pieData: pieData }) {
+class PieRechartComponent extends Component {
 
-  const COLORS = ['#F26D22', 'transparent']
+  COLORS = ['#F26D22', 'transparent']
 
-  return (
-    <>
-      <h2>CPU Usage - Recharts</h2>
-      <ResponsiveContainer width={"100%"} height={300} min-width={300}>
-        <PieChart width={400} height={400}>
-          <text
-            x="50%"
-            y="50%"
-            textAnchor="middle"
-            dominantBaseline="middle"
-            style={{ fontSize: 30, fill: '#999', fontFamily: 'candara' }}
-          >
-            {pieData[0].value}
-          </text>
-          <Pie
-            data={pieData}
-            dataKey="value"
-            innerRadius="80%"
-            outerRadius="100%"
-            fill="#82ca9d"
-            startAngle={-60}
-            endAngle={240}
-            paddingAngle={0}
-            blendStroke
-            cornerRadius={10}
-          >
-            {
-              pieData.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index]} />
-              ))
-            }
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    </>
-  )
-
+  render(){
+     const {pieData} = this.props
+    
+    return (
+      <>
+        <h2>CPU Usage - Recharts</h2>
+        <ResponsiveContainer width={"100%"} height={300} min-width={300}>
+          <PieChart width={400} height={400}>
+            <text
+              x="50%"
+              y="50%"
+              textAnchor="middle"
+              dominantBaseline="middle"
+              style={{ fontSize: 30, fill: '#999', fontFamily: 'candara' }}
+            >
+              {pieData[0].value}
+            </text>
+            <Pie
+              data={pieData}
+              dataKey="value"
+              innerRadius="80%"
+              outerRadius="100%"
+              fill="#82ca9d"
+              startAngle={-60}
+              endAngle={240}
+              paddingAngle={0}
+              blendStroke
+              cornerRadius={10}
+            >
+              {
+                pieData.map((_entry: any, index: number) => (
+                  <Cell key={`cell-${index}`} fill={this.COLORS[index]} />
+                ))
+              }
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </>
+    )
+  }
 }
 
 export default PieRechartComponent 
